55f39b8d94cd1c745b28c2d0042ed4ec6cf2b817
Sumbal Saeed <sumbalsaeed125@gmail.com>
2026-07-23 05:36:49 +0500

    Add chat button functionality and update order button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const orderBtn = document.querySelector(".order-btn");
+const chatNow = document.querySelector(".chat-btn");
 
-document.addEventListener('click', function OrderNow(e)
+orderBtn.addEventListener('click', () =>
 {
-    e.preventDefault();
-
+    window.open("https://www.sukiri.shop/");
 })
+chatNow.addEventListener('click', () => 
+{
+    window.open("https://www.instagram.com/sukiri.co/");
+})
+
